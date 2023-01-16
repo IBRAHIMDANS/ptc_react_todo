f7b46e2ae01f5f99c099ddd9ab6391bd64726d08
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-01-16 11:32:03 +0100

    feat: updating test App minimal test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My Todo/i);
   expect(linkElement).toBeInTheDocument();
 });
