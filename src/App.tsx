161a9b52e6c3b8ba6b6b3866e27a0382ddb33e10
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-01-16 11:31:31 +0100

    feat: updating index and App adding material ui for design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoContextProvider } from './context/TodoContext';
+import { useTodoState } from './hooks/useTodoState';
+import { AddTodo, TodoList } from './components';
+import { uniqueId } from './helpers';
+import { ITodo } from 'types/Todo.types';
 
-function App() {
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+
+const Root = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+const Title = styled(Typography)`
+  margin-bottom: 20px;
+`;
+const PaperStyled = styled(Paper)`
+  min-width: 400px;
+  padding: 1em;
+`;
+
+const defaultValues: ITodo[] = [
+  { id: uniqueId(), description: 'My todo 1', status: false },
+  { id: uniqueId(), description: 'My todo 2', status: true },
+];
+
+const App = () => {
+  const todoState = useTodoState(defaultValues);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+        <TodoContextProvider defaultValues={todoState}>
+          <PaperStyled elevation={3} square>
+            <Title align={'center'} variant={'h4'}> My Todo 😊 </Title>
+            <AddTodo />
+            <TodoList />
+          </PaperStyled>
+        </TodoContextProvider>
+    </Root>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import { ThemeProvider } from '@mui/material';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement =  document.getElementById('root') as HTMLElement
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
